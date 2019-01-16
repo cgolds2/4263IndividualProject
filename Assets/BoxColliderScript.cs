0b49cb8d6f4cc384218ea6dc78e74bd0d630b0a1
Connor Goldsmith <cgolds2@lsu.edu>
2019-01-16 16:16:34 -0600

    Made colliders for places to put the balls

---
+++ Assets/BoxColliderScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxColliderScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseEnter()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse entered " + this.name);
+    }
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        //Debug.Log("Mouse is over " + this.name);
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on " + this.name);
+    }
+
+    void OnMouseDown()
+    {
+        // load a new scene
+        Debug.Log("Mouse clicked " + this.name);
+    }
+
+
+
+}
