9aaa63696d6c38d54894b032367c2ae6ed9fce76
Connor Goldsmith <cgolds2@lsu.edu>
2019-01-16 17:17:37 -0600

    Trying to add animation stuff

--- Assets/BoxColliderScript.cs
+++ Assets/BoxColliderScript.cs
@@ -17,6 +17,10 @@ public class BoxColliderScript : MonoBehaviour {
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
         Debug.Log("Mouse entered " + this.name);
+        var cursorObj = GameObject.Find("CursorObject");
+        cursorObj.transform.position = this.transform.position;
+        cursorObj.transform.AddPos(y: 1);
+
     }
     void OnMouseOver()
     {
@@ -38,4 +42,28 @@ public class BoxColliderScript : MonoBehaviour {
 
 
 
+}
+
+
+public static class Helper
+{
+    public static void SetPos(this Transform transform, float? x = null, float? y = null, float? z = null)
+    {
+        Vector3 position = transform.position;
+
+        if (x.HasValue) position.x = x.Value;
+        if (y.HasValue) position.y = y.Value;
+        if (z.HasValue) position.z = z.Value;
+        transform.position = position;
+    }
+
+    public static void AddPos(this Transform transform, float? x = null, float? y = null, float? z = null)
+    {
+        Vector3 position = transform.position;
+
+        if (x.HasValue) position.x += x.Value;
+        if (y.HasValue) position.y += y.Value;
+        if (z.HasValue) position.z += z.Value;
+        transform.position = position;
+    }
 }
