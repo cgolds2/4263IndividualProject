4808e1aba8d72ae0f97a24f05a5c9b9a37db95a2
Connor Goldsmith <cgolds2@lsu.edu>
2019-01-20 02:38:05 -0600

    Made menu options

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuScript : MonoBehaviour {
-    GUIStyle myBox = new GUIStyle();
-
-    public string seedtoEdit;
+    public static Camera main;
+    public static Camera options;
+    public static Camera credits;
     // Use this for initialization
     void Start()
     {
+      main = GameObject.Find("mainCamera").GetComponent<Camera>();
+    options = GameObject.Find("optionsCamera").GetComponent<Camera>();
+   credits = GameObject.Find("creditsCamera").GetComponent<Camera>();
+    main.enabled = true;
+        options.enabled = false;
+        credits.enabled = false;
     }
 
   
--- Assets/TextButtonScript.cs
+++ Assets/TextButtonScript.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
 
 public class TextButtonScript : MonoBehaviour
 {
 
-    // Use this for initialization
     void Start()
     {
 
     }
+
+  
 
     // Update is called once per frame
     void Update()
@@ -50,14 +53,39 @@ public class TextButtonScript : MonoBehaviour
                 UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
                 break;
             case "Options":
+                MenuScript.main.enabled = false;
+                MenuScript.options.enabled = true;
+                MenuScript.credits.enabled = false;
                 break;
             case "Credits":
+                MenuScript.main.enabled = false;
+                MenuScript.options.enabled = false;
+                MenuScript.credits.enabled = true;
+                break;
+            case "Back":
+                MenuScript.main.enabled = true;
+                MenuScript.options.enabled = false;
+                MenuScript.credits.enabled = false;
+                break;
+            case "File":
+                string path = EditorUtility.OpenFilePanel("Select Python File", "", "py");
+                if (path.Length != 0)
+                {
+                    //TODO: set this to stuff
+                    bool fileContent = File.Exists(path);
+                    var x = Path.GetDirectoryName(path);
+                    var y = Path.GetFileName(path);
+               
+                    
+                }
                 break;
             default:
                 break;
         }
 
     }
+
+
 
 
 }
